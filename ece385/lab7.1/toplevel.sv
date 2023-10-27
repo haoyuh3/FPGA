a9c6502f21135838cf6c2fb9d9b6693ad266f447
haoyuh3 <haoyuh3>
2023-10-27 00:27:06 -0500

    lab7week1

---
+++ ece385/lab7.1/toplevel.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/10/21 10:00:20
+// Design Name: 
+// Module Name: toplevel
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module toplevel( 
+    input logic Clk,
+    input logic reset_rtl_0,
+    //UART
+    input logic uart_rtl_0_rxd,
+    output logic uart_rtl_0_txd,
+    //HDMI
+    output logic hdmi_tmds_clk_n,
+    output logic hdmi_tmds_clk_p,
+    output logic [2:0]hdmi_tmds_data_n,
+    output logic [2:0]hdmi_tmds_data_p, 
+    //HEX displays
+    output logic [7:0] hex_segA,
+    output logic [3:0] hex_gridA,
+    output logic [7:0] hex_segB,
+    output logic [3:0] hex_gridB
+    );
+  
+     mb_block my_block
+       (.HDMI_0_tmds_clk_n(hdmi_tmds_clk_n),
+        .HDMI_0_tmds_clk_p(hdmi_tmds_clk_p),
+        .HDMI_0_tmds_data_n(hdmi_tmds_data_n),
+        .HDMI_0_tmds_data_p(hdmi_tmds_data_p),
+        .clk_100MHz(Clk),
+        .reset_rtl_0(~reset_rtl_0),
+        .uart_rtl_0_rxd(uart_rtl_0_rxd),
+        .uart_rtl_0_txd(uart_rtl_0_txd));
+endmodule
