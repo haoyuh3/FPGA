6a33fc8eba74ed6e2bc138806e65c15a83bf2547
haoyuh3 <haoyuh3>
2023-10-27 00:29:34 -0500

    ip

---
+++ ece385/lab7.1/ip_repo/hdmi_text_controller_1_0/hdl/clk_wiz_0_4/clk_wiz_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2023 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.2.2 (win64) Build 3788238 Tue Feb 21 20:00:34 MST 2023
+-- Date        : Sat Oct 21 11:40:53 2023
+-- Host        : haoyuh running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {d:/vivado
+--               lab/ip_repo/hdmi_text_controller_1_0/src/clk_wiz_0_4/clk_wiz_0_stub.vhdl}
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7s50csga324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,reset,locked,clk_in1";
+begin
+end;
---
+++ ece385/lab7.1/ip_repo/hdmi_text_controller_1_0/src/colormapper.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/10/24 12:04:37
+// Design Name: 
+// Module Name: colormapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+// draw x and draw y is the current place use front to decide whether to draw
+// x15  x,y-> 
+
+
+module colormapper(input  logic [9:0] DrawX, DrawY,
+                       output logic [3:0]  Red, Green, Blue
+
+    );
+    
+endmodule
