6e48e91b2b86084383e0b175a716aa3073cb29bc
haoyuh3 <148392769+haoyuh3@users.noreply.github.com>
2023-10-27 20:31:12 -0500

    Update mb_blink.c

--- ece385/lab6.1/mb_blink.c
+++ ece385/lab6.1/mb_blink.c
@@ -15,20 +15,20 @@
 
 #include "platform.h"
 
-volatile uint32_t* data_out = 0x4000000;  //Hint: either find the manual address (via the memory map in the block diagram, or
-volatile uint32_t* data_a = 0x4001000;															 //replace with the proper define in xparameters (part of the BSP). Either way
-volatile uint32_t* data_b = 0x4001004;																 //this is the base address of the GPIO corresponding to your LEDs
+volatile uint32_t* data_run = 0x4000000;  //Hint: either find the manual address (via the memory map in the block diagram, or
+volatile uint32_t* data_sw = 0x4001000;															 //replace with the proper define in xparameters (part of the BSP). Either way
+volatile uint32_t* data_out = 0x4001004;																 //this is the base address of the GPIO corresponding to your LEDs
 															 //(similar to 0xFFFF from MEM2IO from previous labs).
 // I delete lab6.1 by mistake   just redo my week 1  but without test......... and I use two register for input while one is enough
 int main()
 {
     init_platform();
-	*data_out=0;
+	int16_t sum=0;
 	while(*data_run<1){};
 	while(1+1!=3){
 		int16_t a= *data_a;
-		int16_t b *data_b
-		int16_t data_out= a+b;
+		*sum=sum+a;
+		*data_out=sum;
 		if((a>0 && b>0 && out <0) || ( a<0 && b<0 && out>0 ))
 		while(*data_run<1){}
 		
