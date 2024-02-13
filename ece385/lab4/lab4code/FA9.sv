99c124619f2e1822f7e1a379ec2b808debba77ed
haoyuh3 <148392769+haoyuh3@users.noreply.github.com>
2024-02-12 20:44:00 -0600

    Update FA9.sv

--- ece385/lab4/lab4code/FA9.sv
+++ ece385/lab4/lab4code/FA9.sv
@@ -15,7 +15,7 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: when Cin is high, It function as substracter
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
